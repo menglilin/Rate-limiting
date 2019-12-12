4847d5413c4b8d3451c3177c08bcfcb35ace7cee
linmengli <linmli@10.241.50.151>
2019-12-13 04:43:52 +1100

    Update redisListStore.js
    update the incr function

--- app/lib/redisListStore.js
+++ app/lib/redisListStore.js
@@ -22,7 +22,7 @@ const RedisListStore = function(options) {
             return cb(err);
           }
 
-          return cb();
+          cb();
         });
       } else {
         // if the list equal max, get the earlist timestamp to check if this expired
@@ -38,19 +38,16 @@ const RedisListStore = function(options) {
             return cb(null, resetTime);
           } else {
             // if the earlest timstamp is not expired add the new timestamp and delete the earlest one
-            client.lpush(rdskey, now, (err, res) => {
-              if (err) {
-                return cb(err);
-              }
-
-              client.ltrim(rdskey, 0, options.max - 1, (err, res) => {
+            client
+              .multi()
+              .lpush(rdskey, now)
+              .ltrim(rdskey, 0, options.max - 1)
+              .exec((err, res) => {
                 if (err) {
                   return cb(err);
                 }
-
-                return cb();
+                cb();
               });
-            });
           }
         });
       }
@@ -59,7 +56,7 @@ const RedisListStore = function(options) {
 
   // decrease the request count
   this.decrement = function(key) {
-    const rdskey = predix + key;
+    let rdskey = predix + key;
     client.lpop(rdskey, (err, res) => {
       if (err) {
         return err;
@@ -69,7 +66,7 @@ const RedisListStore = function(options) {
 
   // reset the rate limit
   this.resetKey = function(key) {
-    const rdskey = predix + key;
+    let rdskey = predix + key;
     client.del(rdskey, (err, res) => {
       if (err) {
         return err;
